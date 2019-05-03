14a7bed34185ab64ca2ef15bc39e4c11eb06a084
Mark Story <mark@mark-story.com>
2019-05-02 22:57:15 -0400

    Add job definitions for debugkit

--- jenkins_jobs.groovy
+++ jenkins_jobs.groovy
@@ -1,5 +1,6 @@
 def final AUTHORIZATION_REPO_NAME = 'cakephp/authorization'
 def final AUTHENTICATION_REPO_NAME = 'cakephp/authentication'
+def final DEBUGKIT_REPO_NAME = 'cakephp/debug_kit'
 
 job('Book - Deploy Authorization docs') {
   description('Deploy the authorization docs when changes are pushed.')
@@ -71,3 +72,37 @@ git push -fv dokku HEAD:refs/heads/master
   }
 }
 
+job('Book - Deploy DebugKit docs') {
+  description('Deploy the debugkit docs when changes are pushed.')
+  scm {
+    github(DEBUGKIT_REPO_NAME, 'master')
+  }
+  triggers {
+    scm('H/5 * * * *')
+  }
+  logRotator {
+    daysToKeep(30)
+  }
+  steps {
+    shell('''\
+# Get docs-builder to populate index
+rm -rf docs-builder
+git clone https://github.com/cakephp/docs-builder
+cd docs-builder
+# Build index for each version.
+make populate-index SOURCE="$WORKSPACE" ES_HOST="$ELASTICSEARCH_URL" SEARCH_INDEX_NAME="debugkit-11" SEARCH_URL_PREFIX="/debugkit/1.1"
+cd ..
+
+# Push to dokku
+git remote | grep dokku || git remote add dokku dokku@new.cakephp.org:debugkit-docs
+git push -fv dokku HEAD:refs/heads/master
+    ''')
+  }
+  publishers {
+    slackNotifier {
+      room('#dev')
+      notifyFailure(true)
+      notifyRepeatedFailure(true)
+    }
+  }
+}
