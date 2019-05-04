59f4a9c86aa5499735565db1e03d2d6c7a468a31
Mark Story <mark@mark-story.com>
2019-05-04 16:08:18 -0400

    Add build config for chronos

--- jenkins_jobs.groovy
+++ jenkins_jobs.groovy
@@ -1,6 +1,7 @@
 def final AUTHORIZATION_REPO_NAME = 'cakephp/authorization'
 def final AUTHENTICATION_REPO_NAME = 'cakephp/authentication'
 def final DEBUGKIT_REPO_NAME = 'cakephp/debug_kit'
+def final CHRONOS_REPO_NAME = 'cakephp/chronos'
 
 job('Book - Deploy Authorization docs') {
   description('Deploy the authorization docs when changes are pushed.')
@@ -95,6 +96,41 @@ cd ..
 
 # Push to dokku
 git remote | grep dokku || git remote add dokku dokku@new.cakephp.org:debugkit-docs
+git push -fv dokku HEAD:refs/heads/master
+    ''')
+  }
+  publishers {
+    slackNotifier {
+      room('#dev')
+      notifyFailure(true)
+      notifyRepeatedFailure(true)
+    }
+  }
+}
+
+job('Book - Deploy Chronos docs') {
+  description('Deploy the chronos docs when changes are pushed.')
+  scm {
+    github(CHRONOS_REPO_NAME, 'master')
+  }
+  triggers {
+    scm('H/5 * * * *')
+  }
+  logRotator {
+    daysToKeep(30)
+  }
+  steps {
+    shell('''\
+# Get docs-builder to populate index
+rm -rf docs-builder
+git clone https://github.com/cakephp/docs-builder
+cd docs-builder
+# Build index for each version.
+make populate-index SOURCE="$WORKSPACE" ES_HOST="$ELASTICSEARCH_URL" SEARCH_INDEX_NAME="chronos-1" SEARCH_URL_PREFIX="/chronos/1.x"
+cd ..
+
+# Push to dokku
+git remote | grep dokku || git remote add dokku dokku@new.cakephp.org:chronos-docs
 git push -fv dokku HEAD:refs/heads/master
     ''')
   }
