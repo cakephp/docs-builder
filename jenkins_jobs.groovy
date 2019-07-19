b1d48a5a086fc55f3723657ad3b3a2fec2d1b050
Mark Story <mark@mark-story.com>
2019-07-18 22:42:04 -0400

    Add 2.x jobs for authentication

--- jenkins_jobs.groovy
+++ jenkins_jobs.groovy
@@ -6,8 +6,8 @@ def final DEBUGKIT_REPO_NAME = 'cakephp/debug_kit'
 def final ELASTICSEARCH_REPO_NAME = 'cakephp/elastic-search'
 def final MIGRATIONS_REPO_NAME = 'cakephp/migrations'
 
-job('Book - Deploy Authorization docs') {
-  description('Deploy the authorization docs when changes are pushed.')
+job('Book - Deploy Authorization 1.x docs') {
+  description('Deploy the authorization 1.x docs when changes are pushed.')
   scm {
     github(AUTHORIZATION_REPO_NAME, 'master')
   }
@@ -41,8 +41,43 @@ git push -fv dokku HEAD:refs/heads/master
   }
 }
 
-job('Book - Deploy Authentication docs') {
-  description('Deploy the authentication docs when changes are pushed.')
+job('Book - Deploy Authorization 2.x docs') {
+  description('Deploy the authorization 2.x docs when changes are pushed.')
+  scm {
+    github(AUTHORIZATION_REPO_NAME, '2.x')
+  }
+  triggers {
+    scm('H/5 * * * *')
+  }
+  logRotator {
+    daysToKeep(30)
+  }
+  steps {
+    shell('''\
+# Get docs-builder to populate index
+rm -rf docs-builder
+git clone https://github.com/cakephp/docs-builder
+cd docs-builder
+# Build index for each version.
+make populate-index SOURCE="$WORKSPACE" ES_HOST="$ELASTICSEARCH_URL" SEARCH_INDEX_NAME="authorization-2" SEARCH_URL_PREFIX="/authorization/2.x"
+cd ..
+
+# Push to dokku
+git remote | grep dokku || git remote add dokku dokku@new.cakephp.org:authorization-docs-2
+git push -fv dokku HEAD:refs/heads/master
+    ''')
+  }
+  publishers {
+    slackNotifier {
+      room('#dev')
+      notifyFailure(true)
+      notifyRepeatedFailure(true)
+    }
+  }
+}
+
+job('Book - Deploy Authentication 1.x docs') {
+  description('Deploy the authentication 1.x docs when changes are pushed.')
   scm {
     github(AUTHENTICATION_REPO_NAME, 'master')
   }
@@ -64,6 +99,41 @@ cd ..
 
 # Push to dokku
 git remote | grep dokku || git remote add dokku dokku@new.cakephp.org:authentication-docs
+git push -fv dokku HEAD:refs/heads/master
+    ''')
+  }
+  publishers {
+    slackNotifier {
+      room('#dev')
+      notifyFailure(true)
+      notifyRepeatedFailure(true)
+    }
+  }
+}
+
+job('Book - Deploy Authentication 2.x docs') {
+  description('Deploy the authentication 2.x docs when changes are pushed.')
+  scm {
+    github(AUTHENTICATION_REPO_NAME, '2.x')
+  }
+  triggers {
+    scm('H/5 * * * *')
+  }
+  logRotator {
+    daysToKeep(30)
+  }
+  steps {
+    shell('''\
+# Get docs-builder to populate index
+rm -rf docs-builder
+git clone https://github.com/cakephp/docs-builder
+cd docs-builder
+# Build index for each version.
+make populate-index SOURCE="$WORKSPACE" ES_HOST="$ELASTICSEARCH_URL" SEARCH_INDEX_NAME="authentication-2" SEARCH_URL_PREFIX="/authentication/2.x"
+cd ..
+
+# Push to dokku
+git remote | grep dokku || git remote add dokku dokku@new.cakephp.org:authentication-docs-2
 git push -fv dokku HEAD:refs/heads/master
     ''')
   }
