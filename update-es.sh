2461eec52217704f848f70aa67e33a246d78bfce
Mark Story <mark@mark-story.com>
2021-11-15 00:10:43 -0500

    Fix indexing of build/

--- update-es.sh
+++ update-es.sh
@@ -5,5 +5,5 @@
 # Update elasticsearch indexes.
 for lang in ${LANGS}
 do
-    php /data/populate_search_index.php --source="$SEARCH_SOURCE/$*" --lang="$lang" --host="$ELASTICSEARCH_URL" --url-prefix="$SEARCH_URL_PREFIX"
+    php /data/populate_search_index.php --source="$SEARCH_SOURCE/$lang/$*" --lang="$lang" --host="$ELASTICSEARCH_URL" --url-prefix="$SEARCH_URL_PREFIX"
 done
